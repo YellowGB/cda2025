3b3510f790b98fd26cb2dfdb05226b30499de6ff
YellowGB <yellowchickencorp@gmail.com>
2025-04-09 18:21:55 +0200

    Incredible CSS

--- laravel-common/resources/js/App.vue
+++ laravel-common/resources/js/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="w-[100vw] h-[100vh] bg-amber-200 flex flex-col items-center justify-center">
       <nav>
           <router-link to="/rooms">Rooms</router-link>
       </nav>
